0420756bb5dfa5a4bcf3cef7aeb2be59cc22d277
Altan Orhon <altan@uw.edu>
2024-02-15 17:19:42 -0800

    Reorganized

--- build-and-push/scripts/find-deffile.sh
+++ build-and-push/scripts/find-deffile.sh
@@ -5,7 +5,7 @@ set -eEuo pipefail
 
 # Find the definition file:
 if [[ -z "${DEFFILE:=${INPUT_DEFFILE:-}}" ]]; then
-	FOUND_DEFFILES="$(find "${INPUT_DEFFILES_ROOTDIR:-.}" -type f \( -name 'Apptainer' -or -name 'Singularity' -or -name '*.def' \) -printf '%p\t%f\n' | awk -F$'\t' -v OFS=$'\t' '{print $2 == "Apptainer" ? 1: ($2 == "Singularity" ? 2 : 3), $1}' | awk -F'/' -v OFS=$'\t' '{print NF -1, $0}' | sort --key=1n,2n | cut -f3 | head -n 1 || true)"
+	FOUND_DEFFILES="$(find "${INPUT_DEFFILES_ROOTDIR:-.}" -type f \( -name 'Apptainer' -or -name 'Singularity' -or -name '*.def' \) -printf '%p\t%f\n' | awk -F$'\t' -v OFS=$'\t' '{print $2 == "Apptainer" ? 1: ($2 == "Singularity" ? 2 : 3), $1}' | awk -F'/' -v OFS=$'\t' '{print NF -1, $0}' | sort --key=1n,2n | cut -f3 | head -n 1 | xargs realpath || true)"
 	[[ -z "${FOUND_DEFFILES:-}" ]] && { echo "No definition file found" >&2; exit 1; }
 	mapfile -t deffiles_array <<<"${FOUND_DEFFILES}"
 	echo "Found ${#deffiles_array[@]} definition files:" >&2
